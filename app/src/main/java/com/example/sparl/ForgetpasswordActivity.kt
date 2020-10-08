813411c9a23bae2313df5bf64824787b09f44837
Akash <sparlprojectgithub@gmail.com>
2020-10-08 19:58:08 +0530

    Forget Password

---
+++ app/src/main/java/com/example/sparl/ForgetpasswordActivity.kt
@@ -0,0 +1,19 @@
+package com.example.sparl
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class ForgetpasswordActivity : AppCompatActivity() {
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.example.sparl.R.layout.activity_signup)
+
+
+    }
+}
