c9f056a96e75f01443e96b2b79161a63d65f99d2
Akash <sparlprojectgithub@gmail.com>
2020-11-30 19:54:44 +0530

    Doctor_Profile Page done

---
+++ app/src/main/java/com/example/sparl/Doctor_Profile.kt
@@ -0,0 +1,11 @@
+package com.example.sparl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Doctor_Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doctor__profile)
+    }
+}
