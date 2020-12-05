38ddf23b94b76301daddcbe8c4a8efc24e960f2c
sparlproject <sparlprojectgithub@gmail.com>
2020-12-05 15:54:50 +0530

    hospital profile,clinic search page,hospital search page

--- app/src/main/java/com/example/sparl/Hospital_Profile.kt
+++ app/src/main/java/com/example/sparl/Hospital_Profile.kt
@@ -3,9 +3,9 @@ package com.example.sparl
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Doctor_Profile : AppCompatActivity() {
+class Hospital_Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_doctor_profile)
+        setContentView(R.layout.activity_hospital_profile)
     }
 }
--- app/src/main/java/com/example/sparl/ProfileActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.sparl;
-
-import android.app.Activity;
-
-public class ProfileActivity extends Activity {
-}
