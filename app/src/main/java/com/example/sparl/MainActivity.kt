4cafa026deb482273c6076ba6f6347e35baa8a2f
puneetrewa <puneetrewa999@gmail.com>
2020-12-08 20:52:24 +0530

    Commit after Authentication !!

--- app/src/main/java/com/example/sparl/MainActivity.kt
+++ app/src/main/java/com/example/sparl/MainActivity.kt
@@ -4,22 +4,31 @@ import android.animation.Animator
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.text.TextUtils
 import android.view.View
 import android.view.View.VISIBLE
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+        login()
 
         object : CountDownTimer(5000, 1000) {
             override fun onFinish() {
@@ -36,6 +45,32 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, SignupActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun login(){
+
+        loginButton.setOnClickListener {
+
+            if (TextUtils.isEmpty(email.text.toString())) {
+                email.setError("Please Enter Your Name")
+                return@setOnClickListener
+            } else if (TextUtils.isEmpty(passwordEditText.text.toString())) {
+                passwordEditText.setError("Please Enter Your Email")
+                return@setOnClickListener
+            }
+            auth.signInWithEmailAndPassword(email.text.toString(), passwordEditText.text.toString())
+
+                .addOnCompleteListener{
+                    if(it.isSuccessful){
+                        startActivity(Intent(this@MainActivity, homepage::class.java))
+                        finish()
+                    }
+                    else{
+                        Toast.makeText(this@MainActivity, "Login failed, Please try again!", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+
     }
 
     private fun startAnimation() {
--- app/src/main/java/com/example/sparl/SignupActivity.kt
+++ app/src/main/java/com/example/sparl/SignupActivity.kt
@@ -2,6 +2,7 @@ package com.example.sparl
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.widget.Button
@@ -9,10 +10,17 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_signup.*
 
 
 class SignupActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
+    var databaseReference : DatabaseReference? = null
+    var database: FirebaseDatabase? = null
 
     var Button: Button? = null
 
@@ -29,17 +37,53 @@ class SignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        databaseReference = database?.reference!!.child("profile")
+
+        register()
+     }
+
+    private fun register(){
+
         btnhome.setOnClickListener{
-            val intent = Intent(this, homepage::class.java)
-            startActivity(intent)
+
+            if(TextUtils.isEmpty(input_name.text.toString())){
+                input_name.setError("Please Enter Your Name")
+                return@setOnClickListener
+            }
+            else if(TextUtils.isEmpty(input_email.text.toString())){
+                input_email.setError("Please Enter Your Email")
+                return@setOnClickListener
+            }
+            else if(TextUtils.isEmpty(input_password.text.toString())){
+                input_password.setError("Please Enter Password")
+                return@setOnClickListener
+            }
+            else if(TextUtils.isEmpty(input_reEnterPassword.text.toString())){
+                input_reEnterPassword.setError("Please Enter Password Again")
+                return@setOnClickListener
+            }
+
+            auth.createUserWithEmailAndPassword(input_email.text.toString(),input_password.text.toString())
+
+                .addOnCompleteListener{
+                    if(it.isSuccessful){
+                        val currentUser = auth.currentUser
+                        val currentUserDb = databaseReference?.child((currentUser?.uid!!))
+                        currentUserDb?.child("Name")?.setValue(input_name.text.toString())
+
+                        Toast.makeText(this@SignupActivity, "Registration Success", Toast.LENGTH_LONG).show()
+                        finish()
+                    } else{
+                        Toast.makeText(this@SignupActivity, "Registration failed, Please try again!", Toast.LENGTH_LONG).show()
+                    }
+                }
+
         }
+    }
 
 
 
 
     }
-
-    fun signup() {
-
-    }
-}
