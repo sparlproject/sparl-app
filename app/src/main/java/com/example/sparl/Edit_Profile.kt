31f1708e41d97645bd85f576c7905199a7e69f2a
sparlproject <sparlprojectgithub@gmail.com>
2020-12-10 16:55:27 +0530

    user_profile,about,edit_profile done

---
+++ app/src/main/java/com/example/sparl/Edit_Profile.kt
@@ -0,0 +1,11 @@
+package com.example.sparl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Edit_Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
