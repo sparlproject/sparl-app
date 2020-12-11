8e29215001023cf529ac3a40419391c880f5fdf7
puneetrewa <puneetrewa999@gmail.com>
2020-12-11 09:11:15 +0530

    Merge branch 'master' of C:\Users\Asus\sparl-1 with conflicts.

--- app/src/main/java/com/example/sparl/MainActivity.kt
+++ app/src/main/java/com/example/sparl/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.example.sparl
 
 import android.animation.Animator
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.text.TextUtils
+import android.util.Patterns
 import android.view.View
 import android.view.View.VISIBLE
 import android.view.Window
-import android.view.WindowManager
-import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_forgetpassword.*
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_signup.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,7 +39,12 @@ class MainActivity : AppCompatActivity() {
             override fun onFinish() {
                 bookITextView.visibility = View.GONE
                 loadingProgressBar.visibility = View.GONE
-                rootView.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.colorSplashText))
+                rootView.setBackgroundColor(
+                    ContextCompat.getColor(
+                        this@MainActivity,
+                        R.color.colorSplashText
+                    )
+                )
                 bookIconImageView.setImageResource(R.drawable.background_color_book)
                 startAnimation()
             }
@@ -45,17 +55,80 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, SignupActivity::class.java)
             startActivity(intent)
         }
+
+        btn_forgetpass.setOnClickListener{
+
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Forgot Password")
+            val view = layoutInflater.inflate(R.layout.activity_forgetpassword,null)
+            val username = view.findViewById<EditText>(R.id.input_forgetemail)
+            builder.setView(view)
+            builder.setPositiveButton("Reset", DialogInterface.OnClickListener{_, _ ->
+                      forgetPassword(username)
+            })
+            builder.setNegativeButton("close",DialogInterface.OnClickListener { _, _ ->
+            })
+            builder.show()
+
+        }
+
+        
+
+
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        var mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+
+
+
     }
+
+    private fun forgetPassword(username : EditText) {
+
+        if (username.text.toString().isEmpty()){
+            Toast.makeText(this,"Enter your email address", Toast.LENGTH_SHORT).show()
+
+        }
+        else if (!Patterns.EMAIL_ADDRESS.matcher(username.text.toString()).matches()){
+            Toast.makeText(this,"Enter correct email address", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            auth.sendPasswordResetEmail(username.text.toString())
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Email successfully sent", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(
+                            this,
+                            "Sorry your email is not registered !",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+
+    }
+
+
+
 
     private fun login(){
 
         loginButton.setOnClickListener {
 
             if (TextUtils.isEmpty(email.text.toString())) {
-                email.setError("Please Enter Your Name")
+                email.setError("Please enter your email")
                 return@setOnClickListener
-            } else if (TextUtils.isEmpty(passwordEditText.text.toString())) {
-                passwordEditText.setError("Please Enter Your Email")
+            }
+
+            else if (TextUtils.isEmpty(passwordEditText.text.toString())) {
+                passwordEditText.setError("Please enter your password")
                 return@setOnClickListener
             }
             auth.signInWithEmailAndPassword(email.text.toString(), passwordEditText.text.toString())
@@ -66,7 +139,7 @@ class MainActivity : AppCompatActivity() {
                         finish()
                     }
                     else{
-                        Toast.makeText(this@MainActivity, "Login failed, Please try again!", Toast.LENGTH_LONG).show()
+                        Toast.makeText(this@MainActivity, "Invalid Credentials", Toast.LENGTH_LONG).show()
                     }
                 }
         }
